7828cde960de0322ab38f25e4a97c514a98b0203
Rishat <risatnurullin53@gmail.com>
2022-07-14 17:20:39 +0300

    tфы1

---
+++ tas1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите трёхзначное число: ");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+int digit1 = Number / 10;
+int digit2 = digit1 % 10;
+Console.WriteLine($"Цифра {digit2}");
